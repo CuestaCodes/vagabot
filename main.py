e9a000b7cfb73e710e58be78b38a43a5af9cc067
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-25 20:16:48 +1000

    gitignore update and created TODOS

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 # created 25/06/2022
 
 if __name__ == '__main__':
-    print('hello world!')
+    # open gamestats and save lines
+    file = open('gamestats.log', 'r')
+    lines = file.read().splitlines()
+    file.close()
 
-    # player list for each map
-    # check form [ to next ] to see if it matches player in player list, otherwise next ] for kills
-    # counter for each log for Event_Init, MapChange
+    # TODO: open working csv
 
-    # Individual tracker? Stats by map?
+    # declare counting variables
+    event_num = -1
+    map_num = -1
+    map_current = ""
+
+    for line in lines:
+        # TODO: following for loop tasks
+        # if Event_Init then ++ event_num and break
+        # if MapChange then ++ map_num and update map_current then break
+        # store datetime
+        # if PlayerKilled then store datetime, event_num, map_num, map, player,
+        # 	player_killed as new entry in working csv
+        # if PlayerDied then store datetime, event_num, map_num, player_died
+        pass
+
+# THOUGHTS:
+# player list for each map
+# check form [ to next ] to see if it matches player in player list, otherwise next ] for kills
+# counter for each log for Event_Init, MapChange
+# Individual tracker? Stats by map?
