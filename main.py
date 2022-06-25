d4398f613c4de772bd8ac0886d5d50162e5ac860
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-25 19:32:32 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# created 25/06/2022
+
+if __name__ == '__main__':
+    print('hello world!')
+
+    # player list for each map
+    # check form [ to next ] to see if it matches player in player list, otherwise next ] for kills
+    # counter for each log for Event_Init, MapChange
+
+    # Individual tracker? Stats by map?
