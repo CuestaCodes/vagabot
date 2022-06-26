05b64ad7fe969600884b070192db3b5f3c09df93
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-27 09:13:53 +1000

    prepare create_st()

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # created 25/06/2022
-
 import clean
+import create_streamlit
 
 if __name__ == '__main__':
-    # to be run on one log
-    clean.clean_gamestats()
+    # # to be run on one log
+    # clean.clean_gamestats()
+
+    create_streamlit.create_st()
