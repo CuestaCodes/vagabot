a353a37d3e714132b734e30c92d005e83134a662
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-26 11:26:38 +1000

    extracted mapname

--- clean.py
+++ clean.py
@@ -14,8 +14,9 @@ def clean_gamestats():
 
     # declare current and counting variables
     event_num = -1
-    map_num = -1
-    map_current = ""
+    map_num = None
+    map_current = None
+    players_current = None
 
     for line in lines:
         # TODO: following for loop tasks
@@ -24,18 +25,35 @@ def clean_gamestats():
         if line[:1] != "[":
             continue
 
-        details = line.split("::")
+        details = line.split("::", 1)
 
-        # extract datetime
+        # extract datetime, can be called later
         datetime_details = details[0].split(" ")
         datetime_str = datetime_details[2] + "/" + datetime_details[3] + \
             "/" + datetime_details[5] + " " + datetime_details[4]
         datetime_val = datetime.strptime(datetime_str, "%b/%d/%Y %H:%M:%S")
 
-        event_details = details[1].split(" ", 1)
+        # separate event name from its details
+        event = details[1].split(" ", 1)
+        event_name = event[0]
+        event_details = event[1]
 
-        event = event_details[0]
-        break
+        # keep track of event number
+        if event_name == "Event_Init":
+            event_num = event_num + 1
+            map_num = -1
+            continue
+
+        elif event_name == "Event_LevelInit":
+            map_name = event_details[1:-1]
+            print(map_name)
+
+            map_num = map_num + 1
+            map_current = map_name
+            players_current = set()
+            # continue
+            break
+
         # if Event_Init then ++ event_num and break
         # if MapChange then ++ map_num and update map_current then break
         # store datetime
--- main.py
+++ main.py
@@ -3,4 +3,5 @@
 import clean
 
 if __name__ == '__main__':
+    # to be run on one log
     clean.clean_gamestats()
