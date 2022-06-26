d2b9ec447d56de316731ecc1d88e671487c259ba
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-26 10:56:48 +1000

    extracted datetime

--- clean.py
+++ clean.py
@@ -1,4 +1,7 @@
 # created 26/06/2022
+import pandas as pd
+from datetime import date, datetime
+
 
 def clean_gamestats():
     # open gamestats and save lines
@@ -6,20 +9,36 @@ def clean_gamestats():
     lines = file.read().splitlines()
     file.close()
 
-    # TODO: open working csv
+    # open working csv
+    df = pd.read_csv('gamestats.csv', header=0)
 
-    # declare counting variables
+    # declare current and counting variables
     event_num = -1
     map_num = -1
     map_current = ""
 
     for line in lines:
         # TODO: following for loop tasks
-        # break up lines into parts?
+
+        # use only lines with data
+        if line[:1] != "[":
+            continue
+
+        details = line.split("::")
+
+        # extract datetime
+        datetime_details = details[0].split(" ")
+        datetime_str = datetime_details[2] + "/" + datetime_details[3] + \
+            "/" + datetime_details[5] + " " + datetime_details[4]
+        datetime_val = datetime.strptime(datetime_str, "%b/%d/%Y %H:%M:%S")
+
+        event_details = details[1].split(" ", 1)
+
+        event = event_details[0]
+        break
         # if Event_Init then ++ event_num and break
         # if MapChange then ++ map_num and update map_current then break
         # store datetime
         # if PlayerKilled then store datetime, event_num, map_num, map, player,
         # 	player_killed as new entry in working csv
         # if PlayerDied then store datetime, event_num, map_num, player_died
-        pass
