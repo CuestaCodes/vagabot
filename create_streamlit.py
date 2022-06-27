6cd228d5df5f7fb1c38c43270e449aa94976f99c
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-27 14:00:40 +1000

    showing map kills

--- create_streamlit.py
+++ create_streamlit.py
@@ -54,8 +54,10 @@ def create_st():
         [df_player_only, df_player_killed, df_player_died]).drop_duplicates()
 
     # get ranking df by kills
-    df_player_rank = df_map.groupby(
+    df_player_kills = df_map.groupby(
         ['player'])['player_killed'].count().reset_index(name='kills')
+    df_player_kills_sorted = df_player_kills.sort_values('kills').head(5)
+    df_player_rank = df_player_kills_sorted.index + 1
 
     # hide row index
     # CSS to inject contained in a string
