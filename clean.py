3466077c78c64257f6a660f163757f75133e459b
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-27 16:45:32 +1000

    fixed player kill counts

--- clean.py
+++ clean.py
@@ -38,7 +38,9 @@ def clean_gamestats():
         datetime_details = details[0].split(" ")
         datetime_str = datetime_details[2] + "/" + datetime_details[3] + \
             "/" + datetime_details[5] + " " + datetime_details[4]
-        datetime_val = datetime.strptime(datetime_str, "%b/%d/%Y %H:%M:%S")
+        # casted to string to keep seconds in df, slows clean majorly
+        datetime_val = str(datetime.strptime(
+            datetime_str, "%b/%d/%Y %H:%M:%S"))
 
         # separate event name from its details
         event = details[1].split(" ", 1)
