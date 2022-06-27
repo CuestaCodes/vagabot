56acc696f0bb3c7960d24502c9c6d8333b94c5f2
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-27 13:05:43 +1000

    updated player choice

--- create_streamlit.py
+++ create_streamlit.py
@@ -15,7 +15,9 @@ def create_st():
                        page_icon='🎮', initial_sidebar_state='expanded')
 
     # create drop-down menu for event
-    st.sidebar.markdown("## Select Event Number")
+    st.sidebar.markdown("## Select Event, Map & Player")
+    st.sidebar.markdown(
+        'Select an event number, map and player to see their match statistics.')
     menu_event = st.sidebar.selectbox('Select Event', event_list, index=0)
 
     # reduce dataframe with selected event
@@ -25,17 +27,18 @@ def create_st():
     map_list = df_event['map_key'].tolist()
     map_list_sorted = sorted(set(map_list), key=lambda x: map_list.index(x))
 
+    # create drop-down menu for map and player
+    menu_map = st.sidebar.selectbox('Select Map', map_list_sorted)
+
+    # reduce dataframe with selected map
+    df_map = df[df.map_key == menu_map]
+
     # get player names
-    player_set = set(df_event['player'].dropna())
-    player_killed_set = set(df_event['player_killed'].dropna())
-    player_died_set = set(df_event['player_died'].dropna())
+    player_set = set(df_map['player'].dropna())
+    player_killed_set = set(df_map['player_killed'].dropna())
+    player_died_set = set(df_map['player_died'].dropna())
     player_list = list(player_set)
     player_all_list = list(
         set.union(player_set, player_killed_set, player_died_set))
 
-    # create drop-down menu for map and player
-    st.sidebar.markdown('## Select Map and Player')
-    menu_map = st.sidebar.selectbox('Select Map', map_list_sorted)
     menu_player = st.sidebar.selectbox('Select Player', player_list)
-    st.sidebar.markdown(
-        'Select a map and player to see their match statistics.')
