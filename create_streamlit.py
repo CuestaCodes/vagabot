193d4b24ae77cb2c13e82ad6b991d0bea3076e3d
CuestaCodes <62685253+CuestaCodes@users.noreply.github.com>
2022-06-27 12:02:01 +1000

    created all drop-down menus

--- create_streamlit.py
+++ create_streamlit.py
@@ -14,7 +14,7 @@ def create_st():
     st.set_page_config(page_title='Match Game Statistics',
                        page_icon='🎮', initial_sidebar_state='expanded')
 
-    # create drop-down menu
+    # create drop-down menu for event
     st.sidebar.markdown("## Select Event Number")
     menu_event = st.sidebar.selectbox('Select Event', event_list, index=0)
 
@@ -28,5 +28,13 @@ def create_st():
     player_set = set(df_event['player'].dropna())
     player_killed_set = set(df_event['player_killed'].dropna())
     player_died_set = set(df_event['player_died'].dropna())
-    player_list = list(
+    player_list = list(player_set)
+    player_all_list = list(
         set.union(player_set, player_killed_set, player_died_set))
+
+    # create drop-down menu for map and player
+    st.sidebar.markdown('## Select Map and Player')
+    menu_map = st.sidebar.selectbox('Select Map', map_list)
+    menu_player = st.sidebar.selectbox('Select Player', player_list)
+    st.sidebar.markdown(
+        'Select a map and player to see their match statistics.')
